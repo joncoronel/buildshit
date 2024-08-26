09267acd7c25d234532b4085bc72eece18e10b24
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-08-26 13:24:33 -0700

    starting point

---
+++ app/components/challenges/1/main.js
@@ -0,0 +1,10 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Photo from "@/public/challenges/1/photo.avif";
+import data from "./data.json";
+
+export default function Main() {
+  return <div>Main</div>;
+}
